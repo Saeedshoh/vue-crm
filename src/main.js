200f1bac5770bb7507b58a9ab9a9558e96bac557
Saeedshoh <52253289+Saeedshoh@users.noreply.github.com>
2021-11-20 13:03:52 +0500

    Разделить части сайта от главного контента

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
 import router from './router'
 import store from './store'
+
 
 Vue.config.productionTip = false
 
